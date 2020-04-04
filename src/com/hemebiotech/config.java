33a7fed5d6ca8369440daae55de3244cd5b9f650
Linus Minus <benkhalid.mo@gmail.com>
2020-04-04 11:46:11 +0200

    modif de la classe Write imp dans le main AnalysticsCounter

---
+++ src/com/hemebiotech/config.java
@@ -0,0 +1,22 @@
+package com.hemebiotech;
+
+
+public class config {
+
+	public static String getInputlink() {
+		return inputLink;
+	}
+	public static String getOutputlink() {
+		return outputLink;
+	}
+	
+	public static String outOccurences() {
+		return outputLink;
+	}
+	
+	
+	public static final String inputLink = "C:/Users/Elev/eclipse-workspace-java-se/P2_java/src/symptoms.txt";
+	public static final String outOccurences = "C:/Users/Elev/eclipse-workspace-java-se/P2_java/src/occurences.out";
+	public static final String outputLink = "C:/Users/Elev/eclipse-workspace-java-se/P2_java/src/results.out"; //results.out
+	
+}
